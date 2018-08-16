72973ed22b4620e17126e92bee80087377d85912
shincekbaby <42138520+shincekbaby@users.noreply.github.com>
2018-08-16 20:23:03 +0530

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,19 @@
+//include the library file
+#include<iostream>
+using namespace std;
+//write main function
+int main()
+{//declare and define the primary variables a and b
+int a,b;
+a=6;b=3;
+//declare and define the operations
+int sum=a+b;int diff=a-b;int pro=a*b;float div=a/b;int rem=a%b;
+//print their values
+cout<<"\n sum of a and b="<<sum;
+cout<<"\n difference of a and b="<<diff;
+cout<<"\n product of a and b="<<pro;
+cout<<"\n a divided by b gives "<<div;
+cout<<"\n remainder of a/b="<<rem;
+//end of program
+return 0;
+}
